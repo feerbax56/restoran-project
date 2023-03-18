aa30f9a8b1620419c75fbbf4d65c05bb932d085e
Evseev Anton <soki1997@mail.ru>
2023-03-18 09:40:55 +0500

    add: sign up axios

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,16 +2,39 @@
   <img src="../assets/rest-logo.svg" alt="rest-logo" class="logo">
   <h1>Start!!!!</h1>
   <div class="register">
-    <input type="text" placeholder="Enter Name">
-    <input type="text" placeholder="Enter Email">
-    <input type="text" placeholder="Enter Password">
-    <button>SignUp</button>
+    <input type="text" placeholder="Enter Name" v-model="name">
+    <input type="text" placeholder="Enter Email" v-model="email">
+    <input type="text" placeholder="Enter Password" v-model="password">
+    <button v-on:click="signUp">SignUp</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'SignUp'
+  name: 'SignUp',
+  data() {
+    return {
+      name: '',
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    async signUp() {
+      let result = await axios.post('http://localhost:3000/user', {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+      })
+      console.log(result)
+      if(result.status == 201){
+        alert("OK !!!!")
+        localStorage.setItem('user-info', JSON.stringify(result.data))
+      }
+    }
+  }
 }
 </script>
 
@@ -27,12 +50,13 @@ export default {
   width: 300px;
   margin: 0 auto;
 }
+
 .register input {
   border-radius: 5px;
   border: 1px solid skyblue;
 }
 
-.register button{
+.register button {
   width: 300px;
   height: 40px;
   border: 1px solid skyblue;
