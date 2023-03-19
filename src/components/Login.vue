eda620e8cacf2a6b50350a7772f23f0dc281ac5f
ÐAnton Evseev <soki1997@mail.ru>
2023-03-19 11:05:12 +0500

    add: redirect to homepage

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,7 +4,7 @@
   <div class="login">
     <input type="text" placeholder="Enter Email" v-model="email">
     <input type="text" placeholder="Enter Password" v-model="password">
-    <button>Login</button>
+    <button v-on:click="login">Login</button>
     <p>
       <router-link to="/SignUp">SignUp</router-link>
     </p>
@@ -12,8 +12,35 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'LogIn'
+  name: 'LogIn',
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    async login() {
+      let result = await axios.get(
+          `http://localhost:3000/users?email=${this.email}&password=${this.password}`
+      )
+      if (result.status === 200 && result.data.length > 0) {
+        localStorage.setItem('user-info', JSON.stringify(result.data[0]))
+        this.$router.push({name: 'HomePage'})
+      }
+      console.log(result)
+    }
+  },
+  mounted() {
+    let user = localStorage.getItem('user-info')
+
+    if (user) {
+      this.$router.push({name: 'HomePage'})
+    }
+  },
 }
 
 </script>
