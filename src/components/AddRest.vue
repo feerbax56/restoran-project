3648a31acd07080ea1827cb394e4f4ea44857a97
Evseev Anton <soki1997@mail.ru>
2023-03-27 20:18:12 +0500

    add: input form add rest

--- src/components/AddRest.vue
+++ src/components/AddRest.vue
@@ -1,14 +1,15 @@
-Home.vue<template>
+Home.vue
+<template>
   <Header/>
   <h1>
     Hello username, welcome and Add Restaurant Page
   </h1>
 
   <form action="" class="add">
-    <input type="text" name="name" placeholder="enter name">
-    <input type="text" name="address" placeholder="enter address">
-    <input type="text" name="contact" placeholder="enter contact">
-    <button type="button">Add new Restaurant</button>
+    <input type="text" name="name" placeholder="enter name" v-model="restaurant.name">
+    <input type="text" name="address" placeholder="enter address" v-model="restaurant.address">
+    <input type="text" name="contact" placeholder="enter contact" v-model="restaurant.contact">
+    <button type="button" v-on:click="addRestaurant">Add new Restaurant</button>
   </form>
 </template>
 
@@ -20,6 +21,20 @@ export default {
   name: 'AddRest',
   components: {
     Header
+  },
+  data() {
+    return {
+      restaurant: {
+        name: '',
+        address: '',
+        contact: '',
+      }
+    }
+  },
+  methods: {
+    addRestaurant() {
+      console.log(this.restaurant)
+    }
   },
   mounted() {
     let user = localStorage.getItem('user-info')
